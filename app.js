3952482c8dd08892f1ca5cafce90bf138d5551ad
levi <levi@mscode.dev>
2020-03-03 19:29:51 -0600

    feat: completes exercise to fetch data from api

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const request = require('request')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.sendFile(__dirname +
-  '/index.html'
+app.get('/', (req, response) => {
+request('http://api.coindesk.com/v1/bpi/currentprice.json', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  response.send(`current bitcoin price is $${body.bpi.USD.rate} (${body.bpi.USD.description})`);
+})
+
+})
+
+app.get('/:currency', (req, response) => {
+  request('http://api.coindesk.com/v1/bpi/currentprice.json', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  response.send(`current bitcoin price is $${
+    body.bpi[req.params.currency].rate
+  } (${body.bpi[req.params.currency].description})`);
+})
+})
+
+app.get('/levi', (req, res) => res.sendFile(__dirname +
+  '/levi.html'
 ))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
